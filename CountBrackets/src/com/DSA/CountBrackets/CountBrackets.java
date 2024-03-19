26cf76c50cca8442a6820495bdab0440f25ba62c
Arpitha1212 <141107546+Arpitha1212@users.noreply.github.com>
2024-03-19 21:01:50 +0530

    Corrected-countBrackets

--- CountBrackets/src/com/DSA/CountBrackets/CountBrackets.java
+++ CountBrackets/src/com/DSA/CountBrackets/CountBrackets.java
@@ -1,26 +1,29 @@
 package com.DSA.CountBrackets;
 
 public class CountBrackets {
-	public static void main(String[] args) {
-		String text="(())))";
-		System.out.println("output:"+BracketMatch(text));
-		
-	}
-	static int BracketMatch(String text) {
-		char[] charArray = text.toCharArray();
-		 int count =0;
-		 int ans =0;
-		 int wordlength = charArray.length;
-		 
-		for(int i=0;i<wordlength;i++) {
-			if(charArray[i]=='(') {
-				count++;
-			}
-			else if(charArray[i]==')') {
-				count--;
-			}
-			
-		}
-		return   Math.abs(count);
-	}
+    public static void main(String[] args) {
+        String text = "(())))";
+        int result = BracketMatch(text);
+        System.out.println("output: " + result);
+    }
+
+    static int BracketMatch(String text) {
+        int count = 0;
+        int ans = 0;
+        int n = text.length();
+
+        for (int i = 0; i < n; i++) {
+            if (text.charAt(i) == '(') {
+                count++;
+            } else if (text.charAt(i) == ')') {
+                count--;
+            }
+            if (count < 0) {
+                ans++;
+                count++;
+            }
+        }
+
+        return ans + count;
+    }
 }
